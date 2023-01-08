3a90c16524e2907a4022568fbf8644bec229733d
mobe <mobetax81072@gmail.com>
2023-01-08 23:51:45 +0800

    更新 切換手機版與桌面版選單

--- src/layout/Layouts.tsx
+++ src/layout/Layouts.tsx
@@ -5,6 +5,7 @@ import './Layouts.less';
 import HeaderComponents from '@/layout/components/Header/HeaderComponents';
 import MenuComponent from './components/Menu/MenuComponent';
 import useLayout from '@/hooks/useLayout';
+import { DEVICE } from '@/config/config';
 
 const { Content, Sider } = Layout;
 const WIDTH = 992;
@@ -14,22 +15,46 @@ const Layouts: FC = () => {
     token: { colorBgContainer },
   } = theme.useToken();
 
-  const device = /(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent) ? 'MOBILE' : 'DESKTOP';
-  const { currentLayout,onChangeCollapse} = useLayout();
+  const { currentLayout,setDevice,onChangeCollapse} = useLayout();
   
+ useEffect(()=>{
+  window.onresize = () =>{
+    const rect = document.body.getBoundingClientRect();
+    if(rect.width < WIDTH){
+      setDevice(DEVICE.MOBILE)
+    }
+    else{
+      setDevice(DEVICE.DESKTOP)
+    }
+  }
+ },[])
+
   return (
     <Layout>
       <HeaderComponents></HeaderComponents>
       <Layout>
+        {currentLayout.currentDevice == DEVICE.DESKTOP?(
         <Sider
           breakpoint="lg"
-          collapsedWidth="0"
+          collapsedWidth="80"
           trigger={null}
           collapsible = {true}
           collapsed ={currentLayout.collapsed}
           style={{ backgroundColor: colorBgContainer }}>
           <MenuComponent></MenuComponent>
         </Sider>
+        ):(
+          <Drawer
+            width="200"
+            placement="left"
+            bodyStyle={{ padding: 0, height: '100%' }}
+            closable={false}
+            onClose={onChangeCollapse}
+            open={currentLayout.collapsed}
+          >
+            <MenuComponent></MenuComponent>
+          </Drawer>
+        )}
         <Layout>
           <Content
             style={{
